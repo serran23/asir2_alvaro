883f0546b38e839b4aeeb40119d1c0993cf0513d
serran23 <alsepo_talayuelas@hotmail.com>
2019-09-23 17:26:22 +0200

    Movidas varias

--- asir2.php
+++ asir2.php
@@ -1,10 +1,9 @@
 ﻿Álvaro Serrano
 </br>
-<a href="http://192.168.0.93/asir2.php">Me debes 6k pesetas de whisky -Xanders-</a>
+<a href="http://192.168.0.93/asir2.php">-Xanders-</a>
 </br>
 <a href="http://192.168.0.50/asir2.php">-De la Sen-</a>
 </br>
-<a href="https://m.forocoches.com/foro/forumdisplay.php?f=2">Forocarros</a>
 
 <?php
 $f=fopen('visitas.txt','a');
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+if(isset($_GET['email'])){
+	if (filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) {
+		echo 'Hola, usuario ';
+		print_r($_GET['email']);
+		$f=fopen('suscriptores.txt','a');
+		fwrite($f,$_GET['email']."\r\n");
+		fclose($f);
+	}
+}
+?>
+
+<form>
+	<input name="email"/>
+	<button>Suscribirse</button>
+</form>
