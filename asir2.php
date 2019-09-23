bc5116ad8a75c39bf125ecbcf1ef85af622481e1
serran23 <alsepo_talayuelas@hotmail.com>
2019-09-23 16:20:38 +0200

    Versión inicial

---
+++ asir2.php
@@ -0,0 +1,13 @@
+﻿Álvaro Serrano
+</br>
+<a href="http://192.168.0.93/asir2.php">Me debes 6k pesetas de whisky -Xanders-</a>
+</br>
+<a href="http://192.168.0.50/asir2.php">-De la Sen-</a>
+</br>
+<a href="https://m.forocoches.com/foro/forumdisplay.php?f=2">Forocarros</a>
+
+<?php
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fclose($f)
+?>
