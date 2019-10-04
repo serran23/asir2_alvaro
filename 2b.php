be4148f8d666a0d46fb79477b7c1330783c1409a
serran23 <alsepo_talayuelas@hotmail.com>
2019-10-04 21:28:01 +0200

    Movidas varias

---
+++ 2b.php
@@ -0,0 +1,24 @@
+<?php
+
+function operaciones(){
+	$a=256;
+	$b=128;
+
+	echo 'Primer número='.$a;
+	echo '</br>';
+	echo 'Segundo número='.$b;
+	echo '</br>';
+	echo '</br>';
+	echo 'Suma: '.$a.'+'.$b.'='.($a+$b);
+	echo '</br>';
+	echo 'Resta: '.$a.'-'.$b.'='.($a-$b);
+	echo '</br>';
+	echo 'Multiplicación: '.$a.'x'.$b.'='.($a*$b);
+	echo '</br>';
+	echo 'División: '.$a.'/'.$b.'='.($a/$b);
+	echo '</br>';
+}
+
+operaciones();
+
+?>
