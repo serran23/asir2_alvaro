9969cb1c77c8a3053f21b8f782d23e42e4b73bc1
serran23 <alsepo_talayuelas@hotmail.com>
2019-10-07 15:53:56 +0200

    Todo hecho hasta Unidad 2-Parámetros

---
+++ 2c.php
@@ -0,0 +1,26 @@
+<?php
+
+function operaciones($a,$b){
+	/*
+	$a=256;
+	$b=128;
+	*/
+
+	echo 'Primer número='.$a;
+	echo '</br>';
+	echo 'Segundo número='.$b;
+	echo '</br>';
+	echo '</br>';
+	echo 'Suma: '.$a.'+'.$b.'='.($a+$b);
+	echo '</br>';
+	echo 'Resta: '.$a.'-'.$b.'='.($a-$b);
+	echo '</br>';
+	echo 'Multiplicación: '.$a.'x'.$b.'='.($a*$b);
+	echo '</br>';
+	echo 'División: '.$a.'/'.$b.'='.($a/$b);
+	echo '</br>';
+}
+
+operaciones(256,128);
+
+?>
