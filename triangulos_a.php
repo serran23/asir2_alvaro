b03ab807d2d3e7afbebc41a911f5b60404300ab8
serran23 <alsepo_talayuelas@hotmail.com>
2019-10-11 20:40:38 +0200

    Damos la medida de los lados de un triángulo, y la página nos devuelve si es equilátero, isósceles o escaleno.

---
+++ triangulos_a.php
@@ -0,0 +1,29 @@
+<form>
+	Lado 1    
+	<input name="lado1"/>
+	</br>
+	Lado 2    
+	<input name="lado2"/>
+	</br>
+	Lado 3  
+	<input name="lado3"/>
+	</br>
+	<button>Comprobar</button>
+	</br>
+	</br>
+</form>
+
+<?php
+if(isset($_GET['lado1'], $_GET['lado2'], $_GET['lado3'])){
+	if(is_numeric($_GET['lado1']) && is_numeric($_GET['lado2']) && is_numeric($_GET['lado3']) && ($_GET['lado1'])>0 && ($_GET['lado2'])>0 && ($_GET['lado3'])>0){
+		if($_GET['lado1']==$_GET['lado2'] && $_GET['lado1']==$_GET['lado3']){
+			echo 'Triángulo equilátero.';
+		}elseif($_GET['lado1']==$_GET['lado2'] || $_GET['lado1']==$_GET['lado3'] || $_GET['lado2']==$_GET['lado3']){
+			echo 'Triángulo isósceles.';
+		}else
+			echo 'Triángulo escaleno.';
+	}else
+		echo 'Introduce valores numéricos, por favor.';
+}else
+	echo 'Introduce la medida de los lados del triángulo.';
+?>
