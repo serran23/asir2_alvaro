d28716d45cdd61bceadf4e88913af7d15a85a174
serran23 <alsepo_talayuelas@hotmail.com>
2019-10-14 15:32:48 +0200

    Damos la medida de los ángulos de un triángulo, y la página nos devuelve si es acutángulo, obtusángulo u rectángulo, así como equilátero, isósceles o escaleno.

--- triangulos_c.php
+++ triangulos_c.php
@@ -14,35 +14,51 @@
 </form>
 
 <?php
-if(isset($_GET['angulo1'], $_GET['angulo2'], $_GET['angulo3'])){
-	if(is_numeric($_GET['angulo1']) && is_numeric($_GET['angulo2']) && is_numeric($_GET['angulo3']) && ($_GET['angulo1'])>0 && ($_GET['angulo2'])>0 && ($_GET['angulo3'])>0){
-		if((($_GET['angulo1'])+($_GET['angulo2'])+($_GET['angulo3']))==180){
-			if($_GET['angulo1']==90 || $_GET['angulo2']==90 || $_GET['angulo3']==90){
+if(isset($a1, $a2, $a3)){
+	echo 'Ángulo 1: '.$_GET['angulo1'].'</br>';
+	echo 'Ángulo 2: '.$_GET['angulo2'].'</br>';
+	echo 'Ángulo 3: '.$_GET['angulo3'].'</br>';
+	echo '</br>';
+}else
+?>
+
+<form>
+	<button href="http://localhost/asir2_alvaro/triangulos_c.php">Poner a 0 los valores.</button>
+	</br>
+	</br>
+</form>
+
+<?php
+function loko($a1,$a2,$a3,$l1,$l2,$l3){
+	if(is_numeric($a1) && is_numeric($a2) && is_numeric($a3) && ($a1)>0 && ($a2)>0 && ($a3)>0){
+		if((($a1)+($a2)+($a3))==180){
+			if($a1==90 || $a2==90 || $a3==90){
 				echo 'Triángulo rectángulo.';
-			}elseif($_GET['angulo1']>90 || $_GET['angulo2']>90 || $_GET['angulo3']>90){
+			}elseif($a1>90 || $a2>90 || $a3>90){
 				echo 'Triángulo obtusángulo.';
 			}else
 				echo 'Triángulo acutángulo.';
 		}else
 			echo 'Los ángulos de un triángulo han de sumar 180º, revisa que has metido.';
 	}else
-		echo 'Introduce valores numéricos, por favor.';
-}else
-	echo 'Introduce la medida de los angulos del triángulo.';
+		echo 'Introduce valores numéricos positivos, por favor.';
 
 echo '</br>';
 echo '</br>';
 
-if(isset($_GET['angulo1'], $_GET['angulo2'], $_GET['angulo3'])){
-	if(is_numeric($_GET['angulo1']) && is_numeric($_GET['angulo2']) && is_numeric($_GET['angulo3']) && ($_GET['angulo1'])>0 && ($_GET['angulo2'])>0 && ($_GET['angulo3'])>0){
-		if((($_GET['angulo1'])+($_GET['angulo2'])+($_GET['angulo3']))==180){
-			if($_GET['angulo1']==$_GET['angulo2'] && $_GET['angulo1']==$_GET['angulo3']){
+if(is_numeric($a1) && is_numeric($a2) && is_numeric($a3) && ($a1)>0 && ($a2)>0 && ($a3)>0){
+		if((($a1)+($a2)+($a3))==180){
+			if($a1==$a2 && $a1==$a3){
 				echo 'Triángulo equilátero.';
-			}elseif($_GET['angulo1']==$_GET['angulo2'] || $_GET['angulo1']==$_GET['angulo3'] || $_GET['angulo2']==$_GET['angulo3']){
+			}elseif($a1==$a2 || $a1==$a3 || $a2==$a3){
 				echo 'Triángulo isósceles.';
 			}else
 				echo 'Triángulo escaleno.';
 		}
-	}
+	}else
+		echo 'Introduce valores numéricos positivos, por favor.';
+}
+if(isset($_GET['angulo1'], $_GET['angulo2'], $_GET['angulo3'])){
+	echo loko($_GET['angulo1'],$_GET['angulo2'],$_GET['angulo3'],1,1,1);
 }
 ?>
