f60ad1aeafc0b641aebe8a296ab75b8fb07827a2
serran23 <alsepo_talayuelas@hotmail.com>
2019-11-04 16:08:44 +0100

    Listar los días lectivos del calendario.

---
+++ laboral_a.php
@@ -0,0 +1,56 @@
+<?php
+$start=strtotime('2019-09-11');
+$end=strtotime('2020-05-29');
+$meses=[
+	'01'=>'Enero',
+	'02'=>'Febrero',
+	'03'=>'Marzo',
+	'04'=>'Abril',
+	'05'=>'Mayo',
+	'06'=>'Junio',
+	'07'=>'Julio',
+	'08'=>'Agosto',
+	'09'=>'Septiembre',
+	'10'=>'Octubre',
+	'11'=>'Noviembre',
+	'12'=>'Diciembre',
+];
+
+for($i=$start;$i<=$end;$i+=24*60*60){
+	
+	if ($i==$start){
+		
+		echo 'Comienzo del curso escolar';
+		echo '</br>';
+		echo '</br>';
+		echo 'Septiembre';
+		echo '</br>';
+		echo '</br>';
+		
+	}
+	
+
+	
+	if (date('d', $i)=='01'){
+		
+		echo '</br>';
+		echo $meses[date('m', $i)];
+		echo '</br>';
+		echo '</br>';
+			
+	}
+	
+
+			
+	echo date('Y/m/d', $i);
+	echo '</br>';
+	
+	if ($i==$end){
+		
+		echo '</br>';
+		echo '</br>';
+		echo 'Fin del curso escolar';
+	
+	}
+}
+ ?>
