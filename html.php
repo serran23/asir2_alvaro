ea85b9e2a9170cf7a7989da5c585047252775a13
serran23 <alsepo_talayuelas@hotmail.com>
2019-11-18 15:59:16 +0100

    Fixeos varios.

--- html.php
+++ html.php
@@ -20,26 +20,26 @@
 		<h3>Casi ni título, pero si 2</h3>
 		<h4>fdgfdgsdfsdfsfsd...</h4>
 		<img src="vivafc.jpg" width="400"  height="300"/>
-		</br>
-		</br>
-		<p><font size=4 color="green" face="Comic Sans MS,arial,verdana">Texto random para <u><b>probar que tal van las distintas movidas que tenemos que usar</b></u> y que no se hacer ninguna porque convalidar esta muy chulo pero igual después de 8 años te acuerdas de como buscar las cosas en google y poco, pero yo estoy bien, todo bien, todo correcto, en el proyecto final de curso me van a faltar vírgenes a las que rezar, pero todo biend no pasa onadsd no estoy teniendaoo un ataque socncororo me mueoreoooo!! </br><u><b>Jaja era bromuro dont worry el psicologo y la policía dicen que estoy bien :)</b></u></font></p>
+		<br/>
+		<br/>
+		<p><font size=4 color="green" face="Comic Sans MS,arial,verdana">Texto random para <u><b>probar que tal van las distintas movidas que tenemos que usar</b></u> y que no se hacer ninguna porque convalidar esta muy chulo pero igual después de 8 años te acuerdas de como buscar las cosas en google y poco, pero yo estoy bien, todo bien, todo correcto, en el proyecto final de curso me van a faltar vírgenes a las que rezar, pero todo biend no pasa onadsd no estoy teniendaoo un ataque socncororo me mueoreoooo!! <br/><u><b>Jaja era bromuro dont worry el psicologo y la policía dicen que estoy bien :)</b></u></font><p/>
 		<form input submit></form>
 		<table tr td border 2 cols></table>
-		</br>
-		</br>
+		<br/>
+		<br/>
 		
 		<form>
-		<p>Dato importante 1: <input type="text" name="nombre" size="40"></p>
-		<p>Dato importante 2: <input type="text" name="nacido" min="1900"></p>
+		<p>Dato importante 1: <input type="text" name="nombre" size="40"/></p>
+		<p>Dato importante 2: <input type="text" name="nacido" min="1900"/></p>
 		<p>Variable vital:
-			</br>
-			<input type="radio" name="hm" value="h"> Dato no tan importante, pero casi 1
-			<input type="radio" name="hm" value="m"> Dato no tan importante, pero casi 2
-		 </p>
+			<br/>
+			<input type="radio" name="hm" value="h"/> Dato no tan importante, pero casi 1
+			<input type="radio" name="hm" value="m"/> Dato no tan importante, pero casi 2
+		 <p/>
 		 <p>
 			<input type="submit" value="Enviar">
 			<input type="reset" value="Borrar">
-		  </p>
+		  <p/>
 		</form>
 		
 		<table class="egt" border=1>
