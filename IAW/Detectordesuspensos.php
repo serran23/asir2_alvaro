d9a261b62f5b6a9d57fc7b01f7fb67fdc627ed58
serran23 <55598094+serran23@users.noreply.github.com>
2020-01-14 20:34:07 +0100

    Update Detectordesuspensos.php

--- IAW/Detectordesuspensos.php
+++ IAW/Detectordesuspensos.php
@@ -22,19 +22,20 @@
 
 <?php
 
+//Comprobamos que tenemos los valores metidos, para que nos tire mensajito amistoso si es la 1º vez que entramos
 
 if(isset($_GET['nota']) && isset($_GET['nombre'])){
 	
-/*Comprobamos que tenemos los valores metidos, para que nos tire mensajito amistoso si es la 1º vez que entramos*/
+//Comprobamos que hemos metido un valor entre 0 y 10 mas que nada para que no explote esto. Si no se cumple, mensajito.
 	
 	if(is_numeric($_GET['nota']) && ($_GET['nota'])>=0 && ($_GET['nota'])<=10){
 		
-/*Comprobamos que hemos metido un valor entre 0 y 10 mas que nada para que no explote esto. Si no se cumple, mensajito.*/
+/*Muahahahaha*/
 		
 		if(($_GET['semehacaidounpunto'])=='dead'){
 			$_GET['nota']--;
 			
-/*Muahahahaha*/
+
 			
 			if (($_GET['nota'])>=0 && ($_GET['nota'])<2){
 				$veredicto='muy deficiente';
@@ -43,7 +44,6 @@ if(isset($_GET['nota']) && isset($_GET['nombre'])){
 			
 				$veredicto='suspenso';
 				$comentarioinnecesarioperonecesarioalmismotiempo='no te he visto preguntarme las 100 veces necesarias, al pozo.';
-
 			}elseif (($_GET['nota'])>=5 && ($_GET['nota'])<7){
 				$veredicto='aprobado';
 				$comentarioinnecesarioperonecesarioalmismotiempo='te apruebo porque me da cosa suspenderte, pero vamos...';
@@ -57,9 +57,10 @@ if(isset($_GET['nota']) && isset($_GET['nombre'])){
 				$comentarioinnecesarioperonecesarioalmismotiempo='ejercicio rándom el último día de la evaluación apareció.';
 		}
 				
-		echo 'El alumno/a '.($_GET['nombre']).' ha sacado un '.($_GET['nota']).' lo que viene siendo  un '.$veredicto.', '.$comentarioinnecesarioperonecesarioalmismotiempo;
+			echo 'Nota original: '.(($_GET['nota'])+1).', peeeero...</br>'.'Al final, el alumno/a '.($_GET['nombre']).' ha sacado un '.($_GET['nota']).', lo que viene siendo un '.$veredicto.', '.$comentarioinnecesarioperonecesarioalmismotiempo;
 		
-/*WTF*/
+//Esto pasa cuando tomas mucho monster.
+//Hacemos que mediante un if y elseif's vaya buscando en que rango está nuestra nota, y una vez lo encuentra, metemos el veredicto (y un comentario muy salao) en variables. Una vez termina el if, imprimimos el mensajito con los valores obtenidos
 
 		}else{
 		
@@ -70,7 +71,6 @@ if(isset($_GET['nota']) && isset($_GET['nombre'])){
 				
 					$veredicto='suspenso';
 					$comentarioinnecesarioperonecesarioalmismotiempo='igual hay que dejar de dormir en clase.';
-
 			}elseif (($_GET['nota'])>=5 && ($_GET['nota'])<7){
 					$veredicto='aprobado';
 					$comentarioinnecesarioperonecesarioalmismotiempo='de milagro.';
@@ -83,23 +83,20 @@ if(isset($_GET['nota']) && isset($_GET['nombre'])){
 					$veredicto='sobresaliente';
 					$comentarioinnecesarioperonecesarioalmismotiempo='mu bien.';
 			}
-					
-			echo 'El alumno/a '.($_GET['nombre']).' ha sacado un '.($_GET['nota']).' lo que viene siendo  un '.$veredicto.', '.$comentarioinnecesarioperonecesarioalmismotiempo;
-
-/*Hacemos que mediante un if y elseif's vaya buscando en que rango está nuestra nota, y una vez lo encuentra, metemos el veredicto (y un comentario muy salao) en variables. Una vez termina el if, imprimimos el mensajito con los valores obtenidos*/
+			
+//Imprimimos toda la cadena, donde metemos el nombre del condenado y su nota ajusticiadora.
+			
+			echo 'El alumno/a '.($_GET['nombre']).' ha sacado un '.($_GET['nota']).', lo que viene siendo un '.$veredicto.', '.$comentarioinnecesarioperonecesarioalmismotiempo;
 
 		}
 			
 	}else{
 		
+//Mensaje por si no sabemos ni meter la nota bien.
+		
 		echo 'Ese numero no es bien chingón. Entre 0 y 10, por favor.';
 		
 	}
-
-
 }else
 	echo 'Introduce la nota y el nombre del alumno/a a ajusticiar.';
-
-
-
 ?>
