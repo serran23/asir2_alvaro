bd423343333169fadd1f6bc09c0073f6c6bef0c4
serran23 <55598094+serran23@users.noreply.github.com>
2020-01-29 17:04:52 +0100

    Update Salariomensual.php

--- IAW/Salariomensual.php
+++ IAW/Salariomensual.php
@@ -12,7 +12,7 @@ if($horas>0 && $horas<=40){
 	
 }
 elseif($horas>40){
-	echo 'El trabajador '.$nombre.' ha trabajado '.$horas.' horas este mes, lo que equivale a que toca pagarle '.(12*$horas).'€. Igual hay que plantearse que trabaje menos.';
+	echo 'El trabajador '.$nombre.' ha trabajado '.$horas.' horas este mes, lo que equivale a que toca pagarle '.((($horas-40)*16)+(40*12)).'€. Igual hay que plantearse que trabaje menos.';
 	
 }
 else{
