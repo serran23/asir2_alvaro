231290d74f92ff38bffa6acf1a6ca4c09659d48d
serran23 <55598094+serran23@users.noreply.github.com>
2020-02-06 16:51:51 +0100

    Add files via upload

---
+++ IAW/ConectarBBDD.php
@@ -0,0 +1,9 @@
+﻿<?php 
+
+include ("funcionbienchingona.php");
+
+
+$a=fun($_POST['location'], $_POST['name'], $_POST['pass'], $_POST['bd']);
+
+
+?>
---
+++ IAW/funcionbienchingona.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+function fun($maquina ,$usuario, $contraseña, $bbdd){
+	
+	
+	
+	$asd=new mysqli($maquina ,$usuario, $contraseña, $bbdd);
+	
+	if($asd->connect_error){
+		
+		die('No funchona, fallo número '.$asd->connect_errno);
+		
+	}else{
+		
+		echo 'Conectado desde la funchón. Mu bien.';
+		
+	}
+	
+}
+
+?>
